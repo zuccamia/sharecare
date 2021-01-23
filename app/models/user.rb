51553dda9bd04218ea49c00fbb7132842695dd9c
Ngoc Hoang <zuccamia@gmail.com>
2021-01-23 18:09:19 +0900

    add infos and validations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true, length: { maximum: 30 }
+  validates :last_name, presence: true, length: { maximum: 30 }
+  validates :password, presence: true, confirmation: true
+  validates :age_range, presence: true
 end
