3311f34c3f4382c6415f503a56669cbfc46ae11d
Hongjoo Yang <attinore@gmail.com>
2021-01-23 17:53:41 +0900

    added validation of status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :listing
   validations :start_date, presence: true
   validations :end_date, presence: true
+  validations :status, presence: true
 end
