d727d1e566870160df935ee3c468d1abc0d62e9d
Hongjoo Yang <attinore@gmail.com>
2021-01-28 21:10:48 +0900

    view updated, map updated

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,48 +1,5 @@
 class ListingsController < ApplicationController
-  before_action :call_listing, only: [:show, :create]
   def index
-    @listings = current_user.listings
-  end
-
-  def show
-  end
-
-  def new
-    @listing = Listing.new
-  end
-
-  def create
-    @listing = Listing.new(listing_params)
-    if @listing.save
-      redirect_to @listing, notice: 'Listing was successfully created!'
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @listing.update(listing_params)
-      redirect_to @listing, notice: 'Changes were successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @listing.destroy
-    redirect_to @listing, notice: 'Listing was deleted.'
-  end
-
-  private
-
-  def call_listing
-    @listing = Listing.find(params[:id])
-  end
-
-  def listing_params
-    params.require(:listing).permit(:fee, :service_description, :location, :title)
+    @listings = Listing.all
   end
 end
