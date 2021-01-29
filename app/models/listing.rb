5f270f7bdc922d58e4dc465c13c6e88c89ffd856
Hongjoo Yang <attinore@gmail.com>
2021-01-30 00:15:00 +0900

    styling fix, geo-coding first part

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,8 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
   validates :location, presence: true
   validates :title, presence: true, length: { maximum: 50 }
   validates :fee, presence: true
