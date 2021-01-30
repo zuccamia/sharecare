f2a18e4870f4afe26913f1bc2449ea37acb8397b
Ngoc Hoang <zuccamia@gmail.com>
2021-01-30 12:14:29 +0900

    fix merge conflicts

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,12 +6,8 @@ class Listing < ApplicationRecord
   validates :location, presence: true
   validates :title, presence: true, length: { maximum: 50 }
   validates :fee, presence: true
-<<<<<<< HEAD
-  validates :service_description, presence: true
+  validates :service_description, presence: true, length: { maximum: 500 }
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
-=======
-  validates :service_description, presence: true, length: { maximum: 500 }
->>>>>>> 102cebba347ecdbf5189e76219a83c636bd2dda6
 end
