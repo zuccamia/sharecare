9274ce7172d99a4dacc527806ffd31caf182a084
Ngoc Hoang <zuccamia@gmail.com>
2021-02-04 19:48:45 +0900

    require user to upload photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,5 @@ class User < ApplicationRecord
   validates :password, presence: true, confirmation: true
   validates :age_range, presence: true, inclusion: { in: AGE_RANGE, message: "%{value} is not a valid age range" } 
   has_one_attached :photo
+  validates :photo, presence: true
 end
