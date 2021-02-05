8c1a54203646ab87e5b66c1cab9f76b1af72f46c
Hongjoo Yang <attinore@gmail.com>
2021-02-05 21:06:52 +0900

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -69,15 +69,6 @@ ActiveRecord::Schema.define(version: 2021_02_04_133659) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
-  end
-
   create_table "reviews", force: :cascade do |t|
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
